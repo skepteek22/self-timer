2dce980019d5c7649a34bdf7bee4edd25a6a6ad2
Artem <misterixplay73@gmail.com>
2023-04-08 23:19:16 +0300

    Добавьте файлы проекта.

---
+++ Project1/Source.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <chrono>
+using namespace std;
+int main()
+{
+    
+    auto start_time = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 5000; i++)
+    {
+        cout << i << endl;
+    }
+    auto end_time = chrono::high_resolution_clock::now();
+
+    chrono::duration<double> elapsed_time = end_time - start_time;
+    double seconds = elapsed_time.count();
+
+    cout << "Program execution time: " << seconds << " seconds\n";
+
+    return 0;
+}
